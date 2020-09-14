c4b7f890e4f4a7bfcc7c2ce84e5a332e2e3b7b55
matias <matiaslavalle150396@hotmail.com>
2020-09-14 20:42:18 -0300

    first commit

---
+++ libros y discos/script.js
@@ -0,0 +1,29 @@
+// var nombre = prompt("ingrese su nombre");
+// var edad =  prompt("ingrese su edad");
+// var saludo = "probablemente me caes mal";
+// var listadesuper = ["piña","cebolla","leche","zanahoria","tomate","lechuga","palta"]
+
+// console.log (nombre,apellido,edad)
+
+// alert("tu nombre es " + nombre + " y tu edad es " + edad + "" + saludo)
+
+
+// var persona = {
+//     nombre: "john",
+//     apellido:"snow",
+//     edad:28,
+//     trabajo:"the king on the north",
+// }
+
+var nombre = prompt ("ingrese su Nombre: ")
+ 
+if (nombre = 0){
+    alert ("no escribiste tu name")
+}
+
+
+var apellido = prompt ("ingrese su apellido: ")
+var edad = prompt ("ingrese su edad")
+
+console.log (nombre,apellido,edad)
+
